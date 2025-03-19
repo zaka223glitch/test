86824ad6820c64b354379fab49220f069fe44824
zaka223glitch <kwakwa2321@gmail.com>
2025-03-19 13:44:57 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,9 +1,10 @@
 import unittest
 from app import add
-class TestApp(unittest.Testcase):
-  def test_add(self):
-    self.assertEqual(add(2,3),5)
-    self.assertEqual(add(-1,1),0)
-
-if _name_ == "_main_":
-unittest.main()
+ 
+class TestApp(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+ 
+if __name__ == "__main__":
+    unittest.main()
